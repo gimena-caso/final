f4a6604fa0af163aedae10dc5d406e2c0673e061
Gimena Caso <casogggimena@gmail.com>
2022-09-29 19:22:51 -0300

    Subiendo Trabajo Final

---
+++ index.js
@@ -0,0 +1,16 @@
+function saludar() {
+  var nombre = prompt('Ingrese su nombre por favor:');
+  alert(`Hola,gracias por visitar mi página ${nombre}`)
+}
+
+saludar();
+
+
+let edad;
+edad = prompt("Introduce tu edad");
+if (edad >=18){    
+    alert("Puedes entrar, eres mayor de edad.");
+} else {
+    alert("Eres menor de edad, entras bajo tu responsabilidad.");
+}
+
